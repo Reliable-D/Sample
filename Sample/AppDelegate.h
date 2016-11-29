8aceba4ef42267b4415de57c6aae2f1be9d9e853
dengle <dl1990hz@outlook.com>
2016-11-29 16:22:33 +0800

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by 邓乐 on 2016/11/29.
+//  Copyright © 2016年 Wanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by 邓乐 on 2016/11/29.
+//  Copyright © 2016年 Wanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
